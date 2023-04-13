011b133751896b01c50d2a8fb197ffddc32c5b39
guidota <guido@tamborindeguy.com.ar>
2023-04-13 11:06:12 -0300

    fix(tests): update quests

--- crates/server/src/api/routes/quests/update_quest.rs
+++ crates/server/src/api/routes/quests/update_quest.rs
@@ -14,8 +14,11 @@ use super::types::ToCreateQuest;
 #[derive(Serialize, Deserialize, Debug, ToSchema, Deref)]
 pub struct UpdateQuestRequest(Quest);
 
-#[derive(Serialize, Deserialize, Debug, ToSchema, Deref)]
-pub struct UpdateQuestResponse(Quest);
+#[derive(Serialize, Deserialize, Debug, ToSchema)]
+pub struct UpdateQuestResponse {
+    pub quest: Quest,
+    pub quest_id: String,
+}
 
 #[utoipa::path(
     request_body = UpdateQuestRequest,
@@ -23,7 +26,7 @@ pub struct UpdateQuestResponse(Quest);
         ("quest_id" = String, Path, description = "Quest ID")    
     ),
     responses(
-        (status = 200, description = "Quest updated"),
+        (status = 200, description = "Quest updated", body = UpdateQuestResponse),
         (status = 400, description = "Bad Request"),
         (status = 404, description = "Quest not found"),
         (status = 500, description = "Internal Server Error")
@@ -37,8 +40,12 @@ pub async fn update_quest(
 ) -> HttpResponse {
     let db = data.into_inner();
     let quest_id = quest_id.into_inner();
-    match update_quest_controller(db, quest_id, quest_update.0 .0).await {
-        Ok(quest) => HttpResponse::Ok().json(UpdateQuestResponse(quest)),
+    let quest = quest_update.into_inner();
+    match update_quest_controller(db, quest_id, &quest).await {
+        Ok(quest_id) => HttpResponse::Ok().json(UpdateQuestResponse {
+            quest_id,
+            quest: quest.0,
+        }),
         Err(error) => HttpResponse::from_error(error),
     }
 }
@@ -46,13 +53,12 @@ pub async fn update_quest(
 async fn update_quest_controller<DB: QuestsDatabase>(
     db: Arc<DB>,
     id: String,
-    quest: Quest,
-) -> Result<Quest, QuestError> {
+    quest: &Quest,
+) -> Result<String, QuestError> {
     match quest.is_valid() {
         Ok(_) => db
             .update_quest(&id, &quest.to_create_quest()?)
             .await
-            .map(|_| quest)
             .map_err(|error| error.into()),
         Err(error) => Err(QuestError::QuestValidation(error.to_string())),
     }

8f13236a04efbe4f114bde9aa6fadddf17f84253
guidota <guido@tamborindeguy.com.ar>
2023-05-31 19:25:42 -0300

    feat(benchmark): use signed headers authentication method (#117)
    ## Description
    
    - **[Quests Benchmark]** Send signed headers to authenticate `ws`
    connection
    - Bumps `dcl-crypto-middleware-rs`

--- crates/benchmark/src/client.rs
+++ crates/benchmark/src/client.rs
@@ -1,9 +1,6 @@
-use std::time::{Duration, Instant};
+use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 
-use dcl_crypto::{
-    account::{Account, Signer},
-    AuthChain,
-};
+use dcl_crypto::{account::Account, Expiration, Identity};
 use dcl_rpc::{
     client::RpcClient,
     transports::web_sockets::{
@@ -38,28 +35,39 @@ pub async fn handle_client(
     })?;
 
     let ws = if authenticate {
-        match ws.receive().await {
-            Some(Ok(Message::Text(challenge))) => {
-                let account = Account::random();
-                let signature = account.sign(&challenge);
-                let auth_chain =
-                    AuthChain::simple(account.address(), &challenge, signature.to_string())
-                        .map_err(|_| ClientCreationError::Authentication)?;
-                let auth_chain = serde_json::to_string(&auth_chain)
-                    .map_err(|_| ClientCreationError::Authentication)?;
-                ws.send(Message::Text(auth_chain))
-                    .await
-                    .map_err(|_| ClientCreationError::Authentication)?;
-                ws.clone().ping_every(Duration::from_secs(30)).await;
-                ws
-            }
-            _ => return Err(ClientCreationError::Authentication),
-        }
+        let signer = Account::random();
+        let identity = Identity::from_signer(
+            &signer,
+            Expiration::try_from("3021-10-16T22:32:29.626Z").unwrap(),
+        );
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+
+        let path = "/";
+        let method = "get";
+        let metadata = "{}";
+        let auth_chain = identity.sign_payload(format!("{method}:{path}:{now}:{metadata}"));
+        let signed_headers = format!(
+            r#"{{"X-Identity-Auth-Chain-0": {},  "X-Identity-Auth-Chain-1": {},  "X-Identity-Auth-Chain-2": {}, "X-Identity-Timestamp": {}, "X-Identity-Metadata": {} }}"#,
+            serde_json::to_string(auth_chain.get(0).unwrap()).unwrap(),
+            serde_json::to_string(auth_chain.get(1).unwrap()).unwrap(),
+            serde_json::to_string(auth_chain.get(2).unwrap()).unwrap(),
+            now,
+            "{}"
+        );
+
+        ws.send(Message::Text(signed_headers))
+            .await
+            .map_err(|_| ClientCreationError::Authentication)?;
+
+        ws
     } else {
-        ws.clone().ping_every(Duration::from_secs(30)).await;
         ws
     };
 
+    ws.clone().ping_every(Duration::from_secs(30)).await;
     let transport = WebSocketTransport::new(ws);
 
     let client_connection = Instant::now();

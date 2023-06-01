42e69733cdf151796a8f0b749645a6fda7fd9a53
lauti7 <lautigbustos@gmail.com>
2023-06-01 14:31:14 -0300

    fix: not double verification

--- crates/server/src/api/middlewares/mod.rs
+++ crates/server/src/api/middlewares/mod.rs
@@ -5,6 +5,5 @@ mod tracing;
 
 pub use self::tracing::initialize_telemetry;
 pub use auth::dcl_auth_middleware;
-pub use auth::dcl_optional_auth_middleware;
 pub use metrics::metrics;
 pub use metrics_token::metrics_token;

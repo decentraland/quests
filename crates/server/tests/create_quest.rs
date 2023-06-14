c133f626c7b39f3faf4c87a2ba85282453f11672
lauti7 <lautigbustos@gmail.com>
2023-06-14 11:26:38 -0300

    tests: update tests

--- crates/server/tests/create_quest.rs
+++ crates/server/tests/create_quest.rs
@@ -140,7 +140,7 @@ async fn create_quest_should_be_200_with_reward() {
 }
 
 #[actix_web::test]
-async fn create_quest_should_be_400_quest_validation_error() {
+async fn create_quest_should_be_400_quest_validation_error_missing_definition() {
     let config = get_configuration().await;
     let app = init_service(build_app(&config).await).await;
     let quest_definition = CreateQuestRequest {
@@ -167,7 +167,7 @@ async fn create_quest_should_be_400_quest_validation_error() {
         .append_header(headers[2].clone())
         .append_header(headers[3].clone())
         .append_header(headers[4].clone())
-        .set_json(quest_definition)
+        .set_json(&quest_definition)
         .to_request();
 
     let response = call_service(&app, req).await;
@@ -178,6 +178,175 @@ async fn create_quest_should_be_400_quest_validation_error() {
     assert!(body
         .message
         .contains("Quest Validation Error: Missing the definition for the quest"));
+}
+
+#[actix_web::test]
+async fn create_quest_should_be_400_quest_validation_error_rewards_webhook() {
+    let config = get_configuration().await;
+    let app = init_service(build_app(&config).await).await;
+    let Quest {
+        id: _,
+        creator_address: _,
+        name,
+        description,
+        definition,
+    } = quest_samples::grab_some_apples();
+
+    let create_quest_request = CreateQuestRequest {
+        name,
+        definition: definition.unwrap(),
+        description,
+        reward: Some(QuestReward {
+            hook: QuestRewardHook {
+                webhook_url: "rewards.decentraland.zone/api/campaigns/649c5e38-bef8-4bd6-b13f-bd6a2bdcc096/rewards".to_string(),
+                request_body: Some(HashMap::from([("campaign_key".to_string(), "value-json-webtoken".to_string()), ("beneficiary".to_string(), "{user_address}".to_string())]))
+            },
+            items: vec![QuestRewardItem { name: "SunGlasses".to_string(), image_link: "https://github.com/decentraland".to_string() }]
+        }),
+    };
+
+    let headers = get_signed_headers(
+        create_test_identity(),
+        "post",
+        "/quests",
+        serde_json::to_string(&create_quest_request)
+            .unwrap()
+            .as_str(),
+    );
+
+    let req = TestRequest::post()
+        .uri("/quests")
+        .append_header(headers[0].clone())
+        .append_header(headers[1].clone())
+        .append_header(headers[2].clone())
+        .append_header(headers[3].clone())
+        .append_header(headers[4].clone())
+        .set_json(&create_quest_request)
+        .to_request();
+
+    let response = call_service(&app, req).await;
+
+    assert!(response.status().is_client_error());
+    let body: ErrorResponse = read_body_json(response).await;
+    assert_eq!(body.code, 400);
+    assert!(body
+        .message
+        .contains("Quest Validation Error: Webhook url is not valid"));
+}
+
+#[actix_web::test]
+async fn create_quest_should_be_400_quest_validation_error_rewards_items() {
+    let config = get_configuration().await;
+    let app = init_service(build_app(&config).await).await;
+    let Quest {
+        id: _,
+        creator_address: _,
+        name,
+        description,
+        definition,
+    } = quest_samples::grab_some_apples();
+
+    let mut create_quest_request = CreateQuestRequest {
+        name,
+        definition: definition.unwrap(),
+        description,
+        reward: Some(QuestReward {
+            hook: QuestRewardHook {
+                webhook_url: "https://rewards.decentraland.zone/api/campaigns/649c5e38-bef8-4bd6-b13f-bd6a2bdcc096/rewards".to_string(),
+                request_body: Some(HashMap::from([("campaign_key".to_string(), "value-json-webtoken".to_string()), ("beneficiary".to_string(), "{user_address}".to_string())]))
+            },
+            items: vec![QuestRewardItem { name: "SunGlasses".to_string(), image_link: "github/decentraland".to_string() }]
+        }),
+    };
+
+    let headers = get_signed_headers(
+        create_test_identity(),
+        "post",
+        "/quests",
+        serde_json::to_string(&create_quest_request)
+            .unwrap()
+            .as_str(),
+    );
+
+    let req = TestRequest::post()
+        .uri("/quests")
+        .append_header(headers[0].clone())
+        .append_header(headers[1].clone())
+        .append_header(headers[2].clone())
+        .append_header(headers[3].clone())
+        .append_header(headers[4].clone())
+        .set_json(&create_quest_request)
+        .to_request();
+
+    let response = call_service(&app, req).await;
+
+    assert!(response.status().is_client_error());
+    let body: ErrorResponse = read_body_json(response).await;
+    assert_eq!(body.code, 400);
+    assert!(body
+        .message
+        .contains("Quest Validation Error: Item's image link is not valid"));
+
+    create_quest_request.reward.as_mut().unwrap().items[0].name = "A".to_string();
+    create_quest_request.reward.as_mut().unwrap().items[0].image_link =
+        "https://github.com/decentraland".to_string();
+
+    let headers = get_signed_headers(
+        create_test_identity(),
+        "post",
+        "/quests",
+        serde_json::to_string(&create_quest_request)
+            .unwrap()
+            .as_str(),
+    );
+
+    let req = TestRequest::post()
+        .uri("/quests")
+        .append_header(headers[0].clone())
+        .append_header(headers[1].clone())
+        .append_header(headers[2].clone())
+        .append_header(headers[3].clone())
+        .append_header(headers[4].clone())
+        .set_json(&create_quest_request)
+        .to_request();
+
+    let response = call_service(&app, req).await;
+
+    assert!(response.status().is_client_error());
+    let body: ErrorResponse = read_body_json(response).await;
+    assert_eq!(body.code, 400);
+    assert!(body
+        .message
+        .contains("Quest Validation Error: Item name must be at least 3 characters"));
+
+    create_quest_request.reward.as_mut().unwrap().items = vec![];
+    let headers = get_signed_headers(
+        create_test_identity(),
+        "post",
+        "/quests",
+        serde_json::to_string(&create_quest_request)
+            .unwrap()
+            .as_str(),
+    );
+
+    let req = TestRequest::post()
+        .uri("/quests")
+        .append_header(headers[0].clone())
+        .append_header(headers[1].clone())
+        .append_header(headers[2].clone())
+        .append_header(headers[3].clone())
+        .append_header(headers[4].clone())
+        .set_json(&create_quest_request)
+        .to_request();
+
+    let response = call_service(&app, req).await;
+
+    assert!(response.status().is_client_error());
+    let body: ErrorResponse = read_body_json(response).await;
+    assert_eq!(body.code, 400);
+    assert!(body
+        .message
+        .contains("Quest Validation Error: Reward items must be at least one"));
 }
 
 #[actix_web::test]

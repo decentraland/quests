bd8ab92e71da1416094c9f469ce27d59a21edae0
lauti7 <lautigbustos@gmail.com>
2023-06-14 12:11:21 -0300

    tests: add new tests to check if hook is called

--- crates/protocol/src/rewards.rs
+++ crates/protocol/src/rewards.rs
@@ -9,3 +9,28 @@ pub fn rewards_parser(to_be_parsed: &str, quest_id: &str, user_address: &str) ->
     parsed = parsed.replace(QUEST_ID, quest_id);
     parsed
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn should_parse() {
+        let url = "http://localhost:8080/rewards/{quest_id}/{user_address}";
+        let quest_id = "123";
+        let user_address = "0xB";
+
+        let parsed = super::rewards_parser(url, quest_id, user_address);
+
+        assert_eq!(parsed, "http://localhost:8080/rewards/123/0xB");
+    }
+
+    #[test]
+    fn should_not_change() {
+        let url = "http://localhost:8080/rewards/quest_id";
+        let quest_id = "123";
+        let user_address = "0xB";
+
+        let parsed = super::rewards_parser(url, quest_id, user_address);
+
+        assert_eq!(parsed, "http://localhost:8080/rewards/quest_id");
+    }
+}

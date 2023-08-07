737f74944d21b5de644c7297c8c94c186eaccfcc
lauti7 <lautigbustos@gmail.com>
2023-08-07 19:39:37 -0300

    feat(server): new endpoints to get old quest versions

---
+++ crates/server/src/api/routes/quests/get_quest_updates.rs
@@ -0,0 +1,44 @@
+use crate::domain::quests::QuestError;
+
+use actix_web::{get, web, HttpResponse};
+use quests_db::{core::definitions::QuestsDatabase, Database};
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+
+#[derive(Deserialize, Serialize, ToSchema)]
+pub struct GetQuestUpdatesResponse {
+    pub updates: Vec<String>,
+}
+
+#[utoipa::path(
+    params(
+        ("quest_id" = String, description = "Quest ID")
+    ),
+    responses(
+        (status = 200, description = "IDs of the old quests", body = GetQuestUpdatesResponse),
+        (status = 404, description = "No Update"),
+        (status = 500, description = "Internal Server Error")
+    )
+)]
+#[get("/quests/{quest_id}/updates")]
+pub async fn get_quest_updates(
+    data: web::Data<Database>,
+    quest_id: web::Path<String>,
+) -> HttpResponse {
+    let db = data.into_inner();
+
+    let quest_id = quest_id.into_inner();
+
+    match db.get_all_quest_versions(&quest_id).await {
+        Ok(ids) => {
+            if ids.is_empty() {
+                return HttpResponse::NotFound().finish();
+            }
+            HttpResponse::Accepted().json(GetQuestUpdatesResponse { updates: ids })
+        }
+        Err(err) => {
+            let err: QuestError = err.into();
+            HttpResponse::from_error(err)
+        }
+    }
+}

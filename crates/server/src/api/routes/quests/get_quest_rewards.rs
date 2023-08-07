35e3eb110cbf3a0b8830887bbf668ea0ad70464e
lauti7 <lautigbustos@gmail.com>
2023-08-07 19:39:18 -0300

    feat(server): improve quest rewards endpoint

--- crates/server/src/api/routes/quests/get_quest_rewards.rs
+++ crates/server/src/api/routes/quests/get_quest_rewards.rs
@@ -1,51 +1,128 @@
-use crate::{api::routes::errors::CommonError, domain::quests::QuestError};
-use actix_web::{get, web, HttpResponse};
+use crate::{
+    api::routes::{errors::CommonError, quests::get_user_address_from_request},
+    domain::quests::QuestError,
+};
+use actix_web::{get, web, HttpRequest, HttpResponse};
 use quests_db::{
-    core::definitions::{QuestRewardItem, QuestsDatabase},
+    core::{
+        definitions::{QuestRewardHook, QuestRewardItem, QuestsDatabase},
+        errors::DBError,
+    },
     Database,
 };
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
-use utoipa::ToSchema;
+use tokio::join;
+use utoipa::{IntoParams, ToSchema};
 
 #[derive(Serialize, Deserialize, ToSchema, Debug)]
-pub struct QuestRewards {
+pub struct GetQuestRewardResponse {
     pub items: Vec<QuestRewardItem>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub hook: Option<QuestRewardHook>,
+}
+
+#[derive(Deserialize, IntoParams)]
+pub struct GetQuestRewardsParams {
+    with_hook: bool,
 }
 
 #[utoipa::path(
     responses(
-        (status = 200, description = "Quest Rewards", body = QuestRewards),
+        (status = 200, description = "Quest Rewards", body = GetQuestRewardResponse),
         (status = 404, description = "Not found rewards"),
         (status = 500, description = "Internal Server Error")
     )
 )]
-#[get("/quests/{quest_id}/rewards")]
-pub async fn get_quest_rewards(
+#[get("/quests/{quest_id}/reward")]
+pub async fn get_quest_reward(
+    req: HttpRequest,
     quest_id: web::Path<String>,
     db: web::Data<Database>,
+    query_params: web::Query<GetQuestRewardsParams>,
 ) -> HttpResponse {
     let quest_id = quest_id.into_inner();
     let db = db.into_inner();
 
-    match get_quest_rewards_controller(db, &quest_id).await {
-        Ok(rewards) => HttpResponse::Ok().json(QuestRewards { items: rewards }),
+    let user = get_user_address_from_request(&req).ok();
+
+    match get_quest_rewards_controller(user, db, &quest_id, query_params.with_hook).await {
+        Ok(rewards) => match rewards {
+            Rewards::Items(items) => {
+                HttpResponse::Ok().json(GetQuestRewardResponse { items, hook: None })
+            }
+            Rewards::WithHook { items, hook } => HttpResponse::Ok().json(GetQuestRewardResponse {
+                items,
+                hook: Some(hook),
+            }),
+        },
         Err(error) => HttpResponse::from_error(error),
     }
 }
 
-pub async fn get_quest_rewards_controller<DB: QuestsDatabase>(
+enum Rewards {
+    Items(Vec<QuestRewardItem>),
+    WithHook {
+        items: Vec<QuestRewardItem>,
+        hook: QuestRewardHook,
+    },
+}
+
+async fn get_quest_rewards_controller<DB: QuestsDatabase>(
+    user: Option<String>,
     db: Arc<DB>,
     quest_id: &str,
-) -> Result<Vec<QuestRewardItem>, QuestError> {
-    match db.get_quest_reward_items(quest_id).await {
-        Ok(rewards) => {
-            if rewards.is_empty() {
-                return Err(QuestError::QuestHasNoReward);
-            }
+    mut with_hook: bool,
+) -> Result<Rewards, QuestError> {
+    if let Some(user_address) = user {
+        let quest = db.get_quest(quest_id).await.map_err(QuestError::from)?;
 
-            Ok(rewards)
+        if !user_address.eq_ignore_ascii_case(&quest.creator_address) {
+            with_hook = false
         }
-        Err(_) => Err(QuestError::CommonError(CommonError::Unknown)),
+    } else {
+        with_hook = false
+    }
+
+    if with_hook {
+        let futures = join!(
+            db.get_quest_reward_items(quest_id),
+            db.get_quest_reward_hook(quest_id)
+        );
+        match (futures.0, futures.1) {
+            (Ok(rewards), Ok(hook)) => {
+                if rewards.is_empty() {
+                    return Err(QuestError::QuestHasNoReward);
+                }
+
+                Ok(Rewards::WithHook {
+                    items: rewards,
+                    hook,
+                })
+            }
+            (Err(err), _) => {
+                if matches!(err, DBError::RowNotFound) {
+                    return Err(QuestError::CommonError(CommonError::NotFound));
+                }
+                Err(QuestError::CommonError(CommonError::Unknown))
+            }
+            (_, Err(err)) => {
+                if matches!(err, DBError::RowNotFound) {
+                    return Err(QuestError::CommonError(CommonError::NotFound));
+                }
+                Err(QuestError::CommonError(CommonError::Unknown))
+            }
+        }
+    } else {
+        match db.get_quest_reward_items(quest_id).await {
+            Ok(rewards) => {
+                if rewards.is_empty() {
+                    return Err(QuestError::QuestHasNoReward);
+                }
+
+                Ok(Rewards::Items(rewards))
+            }
+            Err(_) => Err(QuestError::CommonError(CommonError::Unknown)),
+        }
     }
 }

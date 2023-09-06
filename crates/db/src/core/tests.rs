e42949e2d5ae116325841313029b381b4d17ec8f
lauti7 <lautigbustos@gmail.com>
2023-09-06 12:57:20 -0300

    tests: db new queries

--- crates/db/src/core/tests.rs
+++ crates/db/src/core/tests.rs
@@ -70,19 +70,51 @@ pub async fn quest_database_works<DB: QuestsDatabase>(db: &DB, quest: CreateQues
         image_url: quest.image_url,
     };
 
+    // updatable checks
+    assert!(db.is_updatable(&quest_id).await.unwrap());
     let new_quest_id = db
         .update_quest(&quest_id, &updated_quest, "0xA")
         .await
         .unwrap();
-
     let is_active = db.is_active_quest(&quest_id).await.unwrap();
     assert!(!is_active);
+    assert!(!db.is_updatable(&quest_id).await.unwrap());
+    assert!(!db.can_activate_quest(&quest_id).await.unwrap());
+
+    // creators quests should be ONE because query returns current versions (activated and deactivated) and not old versions
+    let quests_by_creator = db.get_quests_by_creator_id("0xA", 0, 50).await.unwrap();
+    assert_eq!(quests_by_creator.len(), 1);
+    assert_eq!(quests_by_creator.get(0).unwrap().id, new_quest_id);
+    assert!(quests_by_creator.get(0).unwrap().active);
+    let create_deactivated_quest = CreateQuest {
+        name: "DEACTIVATED_CREATOR_QUEST",
+        description: quest.description,
+        definition: quest.definition.clone(),
+        image_url: quest.image_url,
+    };
+    let deactivated_quest = db
+        .create_quest(&create_deactivated_quest, "0xA")
+        .await
+        .unwrap();
+    db.deactivate_quest(&deactivated_quest).await.unwrap();
+    // creators quests should be TWO because query returns current versions (activated and deactivated) and not old versions
+    let quests_by_creator = db.get_quests_by_creator_id("0xA", 0, 50).await.unwrap();
+    assert_eq!(quests_by_creator.len(), 2);
+    assert_eq!(quests_by_creator.get(0).unwrap().id, new_quest_id);
+    assert!(quests_by_creator.get(0).unwrap().active);
+    assert_eq!(quests_by_creator.get(1).unwrap().id, deactivated_quest);
+    assert!(!quests_by_creator.get(0).unwrap().active);
+
+    // new quest old versions
+    let old_versions = db.get_all_quest_versions(&new_quest_id).await.unwrap();
+    assert_eq!(old_versions.len(), 1);
+    assert_eq!(old_versions.get(0).unwrap(), &quest_id);
 
     // old quest is still there
-    let get_quest = db.get_quest(&quest_id).await.unwrap();
-    assert_eq!(get_quest.id, quest_id);
-    assert_eq!(get_quest.description, quest.description);
-    assert_eq!(get_quest.definition, quest.definition);
+    let get_old_quest = db.get_quest(&quest_id).await.unwrap();
+    assert_eq!(get_old_quest.id, quest_id);
+    assert_eq!(get_old_quest.description, quest.description);
+    assert_eq!(get_old_quest.definition, quest.definition);
 
     // old quest should not be active
     let active_quests = db.get_active_quests(0, 2).await.unwrap();
@@ -143,6 +175,46 @@ pub async fn quest_database_works<DB: QuestsDatabase>(db: &DB, quest: CreateQues
 
     db.deactivate_quest(&new_quest_id).await.unwrap();
 
+    assert!(db.can_activate_quest(&new_quest_id).await.unwrap());
+    assert!(db.is_updatable(&new_quest_id).await.unwrap());
+
     let active_quests = db.get_active_quests(0, 10).await.unwrap();
     assert_eq!(active_quests.len(), 0);
+
+    assert!(db.activate_quest(&new_quest_id).await.unwrap());
+
+    let active_quests = db.get_active_quests(0, 10).await.unwrap();
+    assert_eq!(active_quests.len(), 1);
+
+    // create quest with TX
+    let items = &[QuestRewardItem {
+        name: "SunGlasses".to_string(),
+        image_link: "https://github.com/decentraland".to_string(),
+    }];
+    let hook = &QuestRewardHook {
+        webhook_url: "https://rewards.webhook.com/{quest_id}".to_string(),
+        request_body: Some(HashMap::from([(
+            "beneficiary".to_owned(),
+            "{user_address}".to_owned(),
+        )])),
+    };
+    let quest_w_reward_id = db
+        .create_quest_with_reward(&quest, "0xB", hook, items)
+        .await
+        .unwrap();
+    let quest_w_reward = db.get_quest(&quest_w_reward_id).await.unwrap();
+    assert_eq!(quest.name, quest_w_reward.name);
+    assert_eq!(quest.description, quest_w_reward.description);
+    assert_eq!(quest.image_url, quest_w_reward.image_url);
+
+    let quest_w_reward_hook = db.get_quest_reward_hook(&quest_w_reward_id).await.unwrap();
+    assert_eq!(quest_w_reward_hook.webhook_url, hook.webhook_url);
+
+    let quest_w_reward_items = db.get_quest_reward_items(&quest_w_reward_id).await.unwrap();
+    assert_eq!(quest_reward_items.len(), 1);
+    assert_eq!(quest_w_reward_items.get(0).unwrap().name, "SunGlasses");
+    assert_eq!(
+        quest_w_reward_items.get(0).unwrap().image_link,
+        "https://github.com/decentraland"
+    )
 }
